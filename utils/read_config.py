3efe72a24a2fba299176e050b4eac2a73bc8467c
praveen <praveen049@yaoo.com>
2017-03-23 13:54:09 +0800

    created loader class object

--- utils/read_config.py
+++ utils/read_config.py
@@ -1,16 +1,28 @@
+#
+#
+# Nokia Copyright...
+############################################################
+
 import json
 
-json_string = '{"product" : "location" , "logtype" : "daily ta"}'
-
 class Config (object):
+    """
+        Create a product configuration object from JSON.
+        Support nested JSON Objects for each product. Currently only one
+        is supported.
+
+       Argument: JSON Object from the configuration file.
+    """
     def __init__(self, attrs):
        self.product = attrs["product"]
        self.logtype = attrs["logtype"]
-       self.datadir = attrs["data_dir"]
        self.server = attrs["server"]
        self.port = attrs["port"]
        self.username = attrs["username"]
        self.password = attrs["password"]
+       self.srcdir = attrs["src_dir"]
+       self.dstdir = attrs["dst_dir"]
+       self.protocol = attrs["protocol"]
 
     def __str__(self):
         return "%s;%s" %(self.product, self.logtype)
@@ -18,10 +30,46 @@ class Config (object):
     def login_info(self):
         return "%s:%s:%s" %(self.server, self.port, self.username)
 
-def load_config(json_object):
+    def get_product(self):
+        return self.product
+
+    def get_logtype(self):
+        return self.logtype
+
+    def get_serverip(self):
+        return self.server
+
+    def get_serverport(self):
+        return self.port
+    
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+
+    def get_srcdir(self):
+        return self.srcdir
+
+    def get_dstdir(self):
+        return self.dstdir
+
+    def get_protocol(self):
+        return self.protocol
+
+class ConfigLoader (object):
+    '''
+        Create a confiuration loaded which can read JSON config files
+    '''
+    def load_config (self, attrs):
+        with open (attrs) as data_file:
+            config = json.load(data_file, object_hook=load_json)
+        return config
+
+def load_json (json_object):
     return Config (json_object)
 
+loader = ConfigLoader()
+config = loader.load_config('../config/product_config.json')
 
-with open ('../config/product_config.json') as data_file:
-    config =  json.load(data_file, object_hook=load_config)
-    print config.login_info()
+print config
