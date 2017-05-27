435ece863a6323ed420bd131ab75f94ff6e1ad00
praveen <praveen049@yaoo.com>
2017-05-27 10:47:27 +0800

    handling multiple folders

--- train.py
+++ train.py
@@ -9,6 +9,11 @@ import math
 import re
 from utils.log2_df import DFCreator
 from utils.read_config import ConfigLoader
+from nltk import word_tokenize
+from nltk import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.lancaster import LancasterStemmer
+from gensim import corpora, models, similarities
 
 def feature_wcount(token, input_df):
     if token == 'wcount':
@@ -24,27 +29,92 @@ def wordcount (log):
     return len(matched)
 
 def expcount (log):
-    matched = re.findall(r'exception|error|failure|warning',log, re.IGNORECASE)
+    matched = re.findall(r'exception|error|failure|warning',log,re.IGNORECASE)
     return len(matched)
 
 def dbgcount (log):
-    matched = re.findall(r'debug|info',log, re.IGNORECASE)
+    matched = re.findall(r'debug|info',log,re.IGNORECASE)
     return len(matched)
 
+
+def apply_cv_nb(input_df, label_s):
+    '''
+    This function will use  Naive Bayes with Count vectorizer
+    '''
+    from sklearn import metrics
+    from sklearn.naive_bayes import MultinomialNB
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.cross_validation import train_test_split
+
+    nd_df = input_df.applymap(keepstring)
+    X = nd_df
+    y = label_s
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    
+    vect = CountVectorizer()
+
+    X_train_dtm = vect.fit_transform(X_train)
+    X_test_dtm = vect.fit_transform(X_test)
+
+    nb = MultinomialNB()
+    nb.fit(X_train_dtm, y_train)
+    y_pred_class = nb.predict(X_test_dtm)
+    metrics.accuracy_score(y_test, y_pred_class)
+
+def keep_nwords(log):
+    e_sw = stopwords.words('english')
+    texts = [word for word in word_tokenize(log.lower().decode('utf-8')) if word not in e_sw]
+    items = texts[0:500]
+    log_begin = ' '.join(map(str, items))
+    return log_begin
+
+def keep_impwords(log):
+    e_sw = stopwords.words('english')
+    texts = [word for word in word_tokenize(log.lower().decode('utf-8')) if word not in e_sw]
+    texts = [impword(word) for word in texts if not hasNumbers(word)]
+    items = texts[0:]
+    log_imp = ''.join(map(str, items))
+    return log_imp
+
+def keep_alpha(log):
+    e_sw = stopwords.words('english')
+    texts = [word for word in word_tokenize(log.lower().decode('utf-8')) if word not in e_sw]
+    texts = [word for word in texts if not hasNumbers(word)]
+    items = texts[0:]
+    log_imp = ' '.join(map(str, items))
+    return log_imp
+
+def hasNumbers(str):
+    return any(c.isdigit() for c in str)
+
+impwords = ['ProxyDiaConnection','debug']
+def impword(w):
+    if w in impwords:
+        return w+' '
+    else:
+        return ''
+
+'''
+Load the configurations
+'''
 loader = ConfigLoader()
 config = loader.load_config('./config/product_config.json')
 
+'''
+Read the logs and convert them into dataframe
+'''
 df_creator = DFCreator()
-case_list = df_creator.get_file_list('./data/logs/')
-
+case_list = df_creator.get_file_list(config.get_dstdir())
 input_df = df_creator.log2_dataframe(case_list, config.get_processlist())
 
+# Replace the NaN with 'empty'
 input_df[input_df.isnull()] = 'empty'
 
 wcount_df = feature_wcount('wcount', input_df)
 expcount_df = feature_wcount('expcount', input_df)
 dbgcount_df = feature_wcount('dbgcount', input_df)
 expcount_df = feature_wcount('expcount', input_df)
-print wcount_df
-print expcount_df
-print dbgcount_df
+print wcount_df.head(4)
+#print expcount_df
+#print dbgcount_df
+input_df.applymap(keep_alpha)
--- utils/log2_df.py
+++ utils/log2_df.py
@@ -2,12 +2,12 @@
 #
 # Nokia Copyright...
 ############################################################
-
 import pandas as pd
 import re
 import os.path
 from glob import glob
 from tensorflow.python.platform import gfile
+from read_config import ConfigLoader
 
 class DFCreator():
     def get_file_list(self, log_dir):
@@ -18,13 +18,14 @@ class DFCreator():
         case_list = {}
         sub_dirs = [x[0] for x in gfile.Walk(log_dir)]
         # The root directory comes first, so skip it.
+        file_list = []
         is_root_dir = True
         for sub_dir in sub_dirs:
-            file_list = []
             if is_root_dir:
                 is_root_dir = False
                 continue
             dir_name = os.path.basename(sub_dir)
+            log_dir = os.path.dirname(sub_dir)
             if dir_name == log_dir:
                continue
             #print("Looking for logs in '" + dir_name + "'")
@@ -32,49 +33,53 @@ class DFCreator():
                 file_glob = os.path.join(log_dir, dir_name, '*.' + extension + "*")
                 file_list.extend(gfile.Glob(file_glob))
             if not file_list:
-                print('No files found')
+               # print('No files found')
                 continue
+            '''
             case_list[dir_name] = {
                 'list' : file_list
                 }
-        return case_list
+            '''
+        return file_list
 
-    def log2_dataframe(self, case_list, process_list):
-        proc_count = len(case_list.keys())
+    def log2_dataframe(self, file_list, process_list):
+        proc_count = len(file_list)
         if proc_count == 0:
             print ('No process log found at')
             return -1
         proc_list = process_list.split(',')
+        proc_list.insert(0,'date')
+        proc_list.insert(1,'case')
         log_df = pd.DataFrame(columns=proc_list)
-        log_proc = pd.DataFrame(columns=['case'])
+        log_proc = pd.DataFrame(columns=['case','date'])
         count = 0
-        for dir_name, file_lists in case_list.items():
-            file_list = file_lists['list']
-            for file in file_list:
-                base = os.path.basename(file)
-                proc_name = os.path.splitext(base)[0]
-                name = re.search(r'(\w+)',proc_name)
-                if name is not None:
-                    proc = name.group(1)
-                    if proc in proc_list:
-                        #print proc
-                        with open(file) as f:
-                            each_log = ''
-                            for each_line in f:
-                                each_log = each_log + each_line
-                            log_df.set_value(count, proc, each_log)
-                            log_proc.loc[count] = [dir_name]
+        for file in file_list:
+            base = os.path.basename(file)
+            dirname =  os.path.dirname(file)
+            base1 = re.search(r'(.*)\/(.*)\/(.*)', dirname).group(3)
+            base2 = re.search(r'(.*)\/(.*)\/(.*)', dirname).group(2)
+            proc_name = os.path.splitext(base)[0]
+            name = re.search(r'(\w+)',proc_name)
+            if name is not None:
+                proc = name.group(1)
+                if proc in proc_list:
+                    with open(file) as f:
+                        each_log = ''
+                        for each_line in f:
+                            each_log = each_log + each_line
+                        log_df.set_value(count, proc, each_log)
+            log_df.set_value(count, 'date', base2)
+            log_df.set_value(count, 'case', base1)
             count = count + 1
-        log_df['case_id'] = log_proc.case
        # print ('Number of logs read into data frame: %d' %(len(log_proc.index)))
        # print ('The name of the data frame %s' %(log_df.shape,))
        # print log_df.columns
-        log_df.set_index(['case_id'], inplace=True)
+        log_df.set_index(['date','case'], inplace=True)
         return log_df
         #print ('Number of logs read into data frame: %d' %(len(log_df.index)))
 
 #loader = ConfigLoader()
-#config = loader.load_config('./config/product_config.json')
+#config = loader.load_config('../config/product_config.json')
 #df_creator = DFCreator()
-#case_list = df_creator.get_file_list('../data/logs/')
-#df_creator.log2_dataframe(case_list, config.get_processlist())
+#file_list = df_creator.get_file_list('../data/')
+#df_creator.log2_dataframe(file_list, config.get_processlist())
